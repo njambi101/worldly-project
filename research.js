ac0e7eb43b022cd34808be7b38cb6a6f6f210b28
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-15 18:51:38 +0300

    project

--- research.js
+++ research.js
@@ -15,11 +15,12 @@ async function fetchAPI(word){
     try {
     infotextEL.style.display = "block";
     meaningEl.style.display = "none";
-    //infotextEL.innerText =`searching for the meaning of the word${word}`
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     const result = await fetch(url).then((res) => res.json())
 
     if(result.title){
+    infotextEL.style.display = "none";
+    meaningEl.style.display = "non";
     title1.innerText = word;
     title2.innerText = "N/A";
     audioEl.src = "none"
@@ -33,7 +34,7 @@ async function fetchAPI(word){
     }
 
 
-    console.log(result);
+    //console.log(result);
     infotextEL.style.display = "none";
     meaningEl.style.display = "block";
 
@@ -48,95 +49,12 @@ async function fetchAPI(word){
 
         infotextEL.innerText ='there has been an error that has happend,please try later'
     }
-    //console.log(word);
+    
 }
 
 inputElm.addEventListener("keyup", (e)=>{
-    //console.log(e.key)
+    
     if(e.target.value && e.key === 'Enter'){
         fetchAPI(e.target.value)
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*fetch('https://api.dictionaryapi.dev/api/v2/entries/en/dictionary')
-    .then(response => {
-
-        if(!response.ok){
-            throw new  error("could not fetch resource");
-        }
-        return response.json();
-
-    })    
-
-    
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-    */
-   /*fetchdata();
-   async function fetchdata(){
-
-    try{
-        //let newword = document.getElementById("js-texts").value
-         const inputElement = document.getElementById("js-texts");
-        const newword = inputElement.value.trim();
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${newword}`);
-        // .trim() removes accidental whitespace
-        if(!response.ok){
-            throw new Error("could not fetch resource");
-        }
-
-        const data = await response.json();
-        console.log(data)
-    }
-    catch(error){
-        console.log(error);
-    }
-   }
-  async function fetchdata() {
-    try {
-        const inputElement = document.getElementById("js-texts");
-        const newword = inputElement.value.trim();
-
-        // Prevent API call if the input is empty
-        if (!newword) {
-            console.warn("Please enter a word to search.");
-            return;
-        }
-
-        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${newword}`);
-        
-        if (!response.ok) {
-            throw new Error("Could not fetch resource (Word not found)");
-        }
-
-        const data = await response.json();
-        console.log(data);
-        //const NEWitem = data.sprites.front_default;
-        //const textElement = document.getElementById('js-paragraph');
-        let definition =data[0].meanings[0].definition[0].definition;
-        //let name =  JSON.parse(JSON.stringify(data));
-         //document.getElementById('js-paragraph').innerHTML = JSON.stringify(data[0].meanings[0].definition);
-         document.getElementById('js-paragraph').innerHTML = definition;
-    } catch (error) {
-        console.error(error);
-        document.getElementById('js-paragraph').innerHTML = "Error" + error.message;
-    }
-}
-
-// Remove the immediate fetchdata(); call if you want it to trigger on user action*/
