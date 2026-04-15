8df40926a59cf8b57f00a7a4212c33536f83e996
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-15 18:38:06 +0300

    project

--- research.js
+++ research.js
@@ -7,6 +7,7 @@ const meaningEl =document.getElementById('meaning-container')
 
 const title1 = document.getElementById("title")
 const title2 = document.getElementById("meaning")
+const audioEl= document.getElementById("audio")
 
 
 
@@ -14,15 +15,38 @@ async function fetchAPI(word){
     try {
     infotextEL.style.display = "block";
     meaningEl.style.display = "none";
-    infotextEL.innerText =`searching for the meaning of the word${word}`
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    //infotextEL.innerText =`searching for the meaning of the word${word}`
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     const result = await fetch(url).then((res) => res.json())
+
+    if(result.title){
+    title1.innerText = word;
+    title2.innerText = "N/A";
+    audioEl.src = "none"
+    }else{
+    infotextEL.style.display = "none";
+    meaningEl.style.display = "block";  
+    audioEl.style.display ="inline-flex";
+    title1.innerText = result[0].word;
+    title2.innerText = result[0].meanings[0].definitions[0].definition;
+    audioEl.src = result[0].phonetics[0].audio;
+    }
+
+
     console.log(result);
     infotextEL.style.display = "none";
     meaningEl.style.display = "block";
+
+    title1.innerText = result[0].word;
+    title2.innerText = result[0].meanings[0].definitions[0].definition;
+    audioEl.src = result[0].phonetics[0].audio;
+
+
+
     } catch (error) {
         console.log(error)
-        
+
+        infotextEL.innerText ='there has been an error that has happend,please try later'
     }
     //console.log(word);
 }
