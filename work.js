640a3328d4d2e557a8d3ae352ef1093dfd42e3d4
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-18 12:47:43 +0300

    create work.js

---
+++ work.js
@@ -0,0 +1,10 @@
+console.log('food is good');
+let http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+ 
+
+
